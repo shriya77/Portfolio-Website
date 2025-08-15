8beacb14a834ff490fcf4336943cb275c350638a
Shriya <shriya@Shriyas-MacBook-Air.local>
2025-08-15 19:00:01 +0800

    file rename

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
-import { AboutSection } from "../components/AboutSection";
+import { AboutSection } from "../components/WorkExperience";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
